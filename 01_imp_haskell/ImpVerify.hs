76d7221ab6ee96ba728c891ee6f29ff27774b997
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-10-10 18:57:23 +0200

    some unit tests added

--- 01_imp_haskell/ImpVerify.hs
+++ 01_imp_haskell/ImpVerify.hs
@@ -1,5 +1,8 @@
 module ImpVerify where
 
+import Test.HUnit (Assertion, (@=?), runTestTT, Test(..), Counts(..))
+import System.Exit (ExitCode(..), exitWith)
+
 type NumType = Int
 type Identifier = String
 data Aexp = Num NumType | Var Identifier | Add Aexp Aexp | Mul Aexp Aexp deriving (Show, Eq)
@@ -60,3 +63,34 @@ checkAtree (Node a@(Mul a1 a2) s m p1 p2) = do
 
 --checkBTree :: Btree -> Maybe (Bexp, State, Bool)
 --checkCtree :: CTree -> Maybe (Com, State, State)
+
+-- unit tests
+
+checkABCTreeTests :: [Test]
+checkABCTreeTests =
+  [ testCase "Atree: Num, correct" $
+    Just ((Num 5), [], 5) @=? checkAtree (Leaf (Num 5) [] 5)
+  , testCase "Atree: Num, incorrect" $
+  	Nothing @=? checkAtree (Leaf (Num 5) [] 2)
+  , testCase "Atree: Var, correct" $
+  	Just ((Var "x"), [("x", 5)], 5) @=? checkAtree (Leaf (Var "x") [("x", 5)] 5)
+  , testCase "Atree: Var, incorrect (other value)" $
+  	Nothing @=? checkAtree (Leaf (Var "x") [("x", 1)] 5)
+  , testCase "Atree: Var, incorrect (not found)" $
+  	Nothing @=? checkAtree (Leaf (Var "x") [] 5)
+  ]
+
+
+
+-- testing stuff
+
+testCase :: String -> Assertion -> Test
+testCase label assertion = TestLabel label (TestCase assertion)
+
+exitProperly :: IO Counts -> IO ()
+exitProperly m = do
+  counts <- m
+  exitWith $ if failures counts /= 0 || errors counts /= 0 then ExitFailure 1 else ExitSuccess
+
+main :: IO ()
+main = exitProperly (runTestTT (TestList checkABCTreeTests))
