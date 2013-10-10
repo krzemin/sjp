887aebbf7dc592f493c1dc968260998ddc41c671
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-10-10 17:43:38 +0200

    lookupState and insertState implemented

--- 01_imp_haskell/ImpVerify.hs
+++ 01_imp_haskell/ImpVerify.hs
@@ -1,9 +1,19 @@
 module ImpVerify where
 
-
+type Numeral = Int
 type Identifier = String
-data Aexp = Num Int | Var Identifier | Add Aexp Aexp | Mul Aexp Aexp
-data Bexp = T | F | And Bexp Bexp | Or Bexp Bexp | Not Bexp | Eq Aexp Aexp | Leq Aexp Aexp
-data Com = Skip | Assign Identifier Aexp | Seq Com Com | If Bexp Com Com | While Bexp Com
+data Aexp = Num Numeral | Var Identifier | Add Aexp Aexp | Mul Aexp Aexp deriving (Show, Eq)
+data Bexp = T | F | And Bexp Bexp | Or Bexp Bexp | Not Bexp | Eq Aexp Aexp | Leq Aexp Aexp deriving (Show, Eq)
+data Com = Skip | Assign Identifier Aexp | Seq Com Com | If Bexp Com Com | While Bexp Com deriving (Show, Eq)
 
-type State = [(Identifier, Int)]
+type State = [(Identifier, Numeral)]
+
+lookupState :: Identifier -> State -> Maybe Numeral
+lookupState = lookup
+
+insertState :: (Identifier, Numeral) -> State -> State
+insertState (x, n) [] = [(x, n)]
+insertState (x, n) ((y, m) : s)
+	| x == y = (x, n) : s
+	| otherwise = (y, m) : (insertState (x, n) s)
+
