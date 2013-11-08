6a2de7f560454262ccddc8c220ef7b70a3301dac
Piotr Krzemiński <pio.krzeminski@gmail.com>
2013-11-08 10:30:43 +0100

    abstract machine implementation finished

--- 05_imp_haskell/ImpAbsMachine.hs
+++ 05_imp_haskell/ImpAbsMachine.hs
@@ -2,21 +2,58 @@ module ImpAbsMachine where
 
 import ImpAst
 import ImpMemory
+import Data.Maybe
+
+data StackElem = Add1 Aexp
+				| Add0 Numeral
+				| Neg
+				| And1 Bexp
+				| Or1 Bexp
+				| Asn Identifier
+				| Cont Com
+				| Sel Com Com
+				deriving (Show, Eq)
+
+data Current = A Aexp | B Bexp | C Com
+				deriving (Show, Eq)
+
+stepMachine :: (Current, [StackElem], State) -> (Current, [StackElem], State)
+
+stepMachine (A (Var x), st, s) = (A (Num (fromJust $ lookupState x s)), st, s)
+
+stepMachine (A (Num n1), (Add0 n0):st, s) = (A (Num (n0 + n1)), st, s)
+stepMachine (A (Num n0), (Add1 a1):st, s) = (A a1, (Add0 n0):st, s)
+stepMachine (A (Add a0 a1), st, s) = (A a0, (Add1 a1):st, s)
 
 
-data StackElem = Add1 Aexp | Add0 Numeral
+stepMachine (B (Not b), st, s) = (B b, Neg:st, s)
+stepMachine (B T, Neg:st, s) = (B F, st, s)
+stepMachine (B F, Neg:st, s) = (B T, st, s)
 
-stepMachine :: (Aexp, [StackElem], State) -> (Aexp, [StackElem], State)
+stepMachine (B (And b0 b1), st, s) = (B b0, (And1 b1):st, s)
+stepMachine (B F, (And1 _):st, s) = (B F, st, s)
+stepMachine (B T, (And1 b1):st, s) = (B b1, st, s)
 
-stepMachine (Var x, st, s) = (Num (fromJust $ lookupState x s), st, s)
-
-stepMachine (Num n1, (Add0 n0):st, s) = (Num (n0 + n1), st, s)
-
-stepMachine (Num n0, (Add1 a1):st, s) = (a1, (Add0 n0):st, s)
-
-
+stepMachine (B (Or b0 b1), st, s) = (B b0, (Or1 b1):st, s)
+stepMachine (B T, (Or1 _):st, s) = (B T, st, s)
+stepMachine (B F, (Or1 b1):st, s) = (B b1, st, s)
 
 
+stepMachine (C (Assign x a), st, s) = (A a, (Asn x):st, s)
+stepMachine (A (Num n), (Asn x):st, s) = (C Skip, st, insertState (x, n) s) 
+
+stepMachine (C (Seq c1 c2), st, s) = (C c1, (Cont c2):st, s)
+stepMachine (C Skip, (Cont c2):st, s) = (C c2, st, s)
+
+stepMachine (C (If b c1 c2), st, s) = (B b, (Sel c1 c2):st, s)
+stepMachine (B T, (Sel c1 _):st, s) = (C c1, st, s)
+stepMachine (B F, (Sel _ c2):st, s) = (C c2, st, s)
+
+stepMachine (C (While b c), st, s) = (C (If b (Seq c (While b c)) Skip), st, s)
+
+main = do
+	putStrLn $ show $ stepMachine (A $ Add (Num 2) (Var "x"), [], [("x", 10)])
+	putStrLn $ show $ stepMachine (A $ (Num 2), [Asn "x"], [("x", 10)])
 
 
 
