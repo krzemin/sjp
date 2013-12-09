626b1122e22d98e6567a8a635565cfe26b5e0089
Piotr Krzeminski <pio.krzeminski@gmail.com>
2013-12-09 23:01:24 +0100

    denotational semantics for part of a language On branch master Your branch is up-to-date with 'origin/master'.
    Changes to be committed:
      (use "git reset HEAD <file>..." to unstage)
    
    	modified:   09_imp_environments/ImpEnv.hs

--- 09_imp_environments/ImpEnv.hs
+++ 09_imp_environments/ImpEnv.hs
@@ -2,11 +2,11 @@ module ImpEnv where
 
 import Data.Maybe
 
-type Identifier = String
-type ProcName = Identifier
+type IdeV = String
+type IdeP = String
 
 data Aexp = Num Int
-          | Var Identifier
+          | Var IdeV
           | Add Aexp Aexp
           deriving (Show, Eq)
 
@@ -17,37 +17,72 @@ data Bexp = T
           deriving (Show, Eq)
 
 data Com = Skip
-         | Assign Identifier Aexp
+         | Assign IdeV Aexp
          | Seq Com Com
          | If Bexp Com Com
          | While Bexp Com
          | Block [DeclV] [DeclP] Com
-         | Call ProcName Aexp
+         | Call IdeP Aexp
          deriving (Show, Eq)
 
-data DeclV = Dim Identifier Aexp deriving (Show, Eq)
-data DeclP = Proc ProcName Identifier Com deriving (Show, Eq)
+data DeclV = Dim IdeV Aexp deriving (Show, Eq)
+data DeclP = Proc IdeP IdeV Com deriving (Show, Eq)
 
-type State = [(Identifier, Int)]
+type Loc = Int
+data Store = Sto [(Loc, Int)] Loc -- memory and next free location
+type EnvV = [(IdeV, Loc)]
+type EnvP = [(IdeP, Loc)]
 
-lookupState :: Identifier -> State -> Maybe Int
-lookupState = lookup
+-- memory management
+new :: Loc -> Loc
+new n = n + 1 
 
-insertState :: (Identifier, Int) -> State -> State
-insertState (x, n) lst = (x, n) : lst
+lookupEnvV :: EnvV -> IdeV -> Maybe Loc
+lookupEnvV envV v = lookup v envV
 
+lookupV :: EnvV -> Store -> IdeV -> Maybe Int
+lookupV envV (Sto sto _) v = do
+  loc <- lookupEnvV envV v
+  lookup loc sto
 
-evalAexp :: Aexp -> State -> Int
-evalAexp (Num n) _ = n
-evalAexp (Var x) s = fromJust $ lookupState x s
-evalAexp (Add a0 a1) s = evalAexp a0 s + evalAexp a1 s
+updateSto :: Loc -> Int -> Store -> Store
+updateSto loc val (Sto sto next)  = Sto sto' next
+  where
+    sto' = update sto
+    update ((x, v) : rest)
+      | x == loc = (loc, val) : rest
+      | otherwise = (x, v) : update rest
 
-evalBexp :: Bexp -> State -> Bool
-evalBexp T _ = True
-evalBexp F _ = False
-evalBexp (Not b) s = not $ evalBexp b s
-evalBexp (Leq a0 a1) s = evalAexp a0 s <= evalAexp a1 s
+-- denotational semantics
 
+evalAexp :: Aexp -> EnvV -> Store -> Int
+evalAexp (Num n) _ _ = n
+evalAexp (Var x) envV sto = fromJust $ lookupV envV sto x
+evalAexp (Add a0 a1) envV sto = evalAexp a0 envV sto + evalAexp a1 envV sto
+
+evalBexp :: Bexp -> EnvV -> Store -> Bool
+evalBexp T _ _ = True
+evalBexp F _ _ = False
+evalBexp (Not b) envV sto = not $ evalBexp b envV sto
+evalBexp (Leq a0 a1) envV sto = evalAexp a0 envV sto <= evalAexp a1 envV sto
+
+evalCom :: Com -> EnvV -> Store -> Store
+evalCom Skip _ sto = sto
+evalCom (Assign x a) envV sto = updateSto loc val sto
+  where
+    loc = fromJust $ lookupV envV sto x
+    val = evalAexp a envV sto
+evalCom (Seq c0 c1) envV sto = evalCom c1 envV $ evalCom c0 envV sto
+evalCom (If b c0 c1) envV sto =
+  if evalBexp b envV sto
+    then evalCom c0 envV sto
+    else evalCom c1 envV sto
+evalCom (While b c) envV sto = fix fun sto
+  where
+    fun :: (Store -> Store) -> Store -> Store
+    fun g st = if evalBexp b envV st then g (evalCom c envV st) else st 
+    fix :: (a -> a) -> a
+    fix f = let r = f r in r
 
 
 
