460fa5c5fddbc157b6ac9910c0967f4619835fea
Piotr Krzeminski <pio.krzeminski@gmail.com>
2014-02-05 01:36:24 +0100

    not type-checking yet, but some progress applied

--- 14_fun_haskell/LazyFun.hs
+++ 14_fun_haskell/LazyFun.hs
@@ -33,22 +33,24 @@ type Cont = Val -> Val'
 
 data Val =  VN Int
           | VB Bool
-          | VFun (Val -> Cont -> Val')
+          | VFun (Val' -> Cont -> Val')
           | VSum (Expr, Expr)
           | VInl Expr
           | VInr Expr
+          | VExpr Expr
 
 instance Show Val where
   show (VN n) = show n
   show (VB b) = show b
   show (VFun _) = "@function"
   show (VSum p) = show p
-  show (VInl v) = "inl@" ++ show v
-  show (VInr v) = "inr@" ++ show v
+  show (VInl e) = "inl@" ++ show e
+  show (VInr e) = "inr@" ++ show e
+  show (VExpr e) = "code@" ++ show e
 
 data Val' = OK Val | Err | TypeErr String deriving (Show)
 
-type Env = Map Ide Val
+type Env = Map Ide Val'
 
 typedN :: (Int -> Val') -> Val -> Val'
 typedN f (VN x) = f x
@@ -58,7 +60,7 @@ typedB :: (Bool -> Val') -> Val -> Val'
 typedB f (VB x) = f x
 typedB _ _ = TypeErr "expected bool"
 
-typedF :: ((Val -> Cont -> Val') -> Val') -> Val -> Val'
+typedF :: ((Val' -> Cont -> Val') -> Val') -> Val -> Val'
 typedF f (VFun x) = f x
 typedF _ _ = TypeErr "expected fun"
 
@@ -71,6 +73,10 @@ typedLR f (VInl x) = f x
 typedLR f (VInr x) = f x
 typedLR _ _ = TypeErr "expected inl or inr"
 
+star :: (Val -> Val') -> Val' -> Val'
+star f (OK x) = f x
+star _ Err = Err
+star _ (TypeErr x) = TypeErr x
 
 evalExpr :: Expr -> Env -> Cont -> Val'
 evalExpr (N n) _ k = k (VN n)
@@ -96,25 +102,21 @@ evalExpr (If e e1 e2) env k = evalExpr e env (typedB k')
         k' False = evalExpr e2 env k
 evalExpr (Var x) env k = case lookup x env of
   Nothing -> Err
-  Just v -> k v
+  Just v -> star k v
 evalExpr (Lam x e) env k = k (VFun f)
   where f v = evalExpr e (insert x v env) 
 evalExpr (App e1 e2) env k = evalExpr e1 env (typedF k')
   where k' f = evalExpr e2 env (`f` k)
-evalExpr (Pair e1 e2) env k = evalExpr e1 env k'
-  where k' v1 = evalExpr e2 env (k'' v1)
-        k'' v1 v2 = k (VSum (v1, v2))
+evalExpr (Pair e1 e2) env k = k (VSum (e1, e2))
 evalExpr (Proj1 e) env k = evalExpr e env (typedS k')
-  where k' (v1, _) = k v1
+  where k' (e1, _) = evalExpr e1 env k
 evalExpr (Proj2 e) env k = evalExpr e env (typedS k')
-  where k' (_, v2) = k v2
-evalExpr (Inl e) env k = evalExpr e env k'
-  where k' v = k (VInl v)
-evalExpr (Inr e) env k = evalExpr e env k'
-  where k' v = k (VInr v)
+  where k' (_, e2) = evalExpr e2 env k
+evalExpr (Inl e) env k = k (VInl e)
+evalExpr (Inr e) env k = k (VInr e)
 evalExpr (Case e x1 e1 x2 e2) env k = evalExpr e env (typedLR k')
-  where k' (VInl v1) = evalExpr e1 (insert x1 v1 env) k
-        k' (VInr v2) = evalExpr e2 (insert x2 v2 env) k
+  where k' (VInl e1') = evalExpr e1 (insert x1 (VExpr e1') env) k
+        k' (VInr e2') = evalExpr e2 (insert x2 (VExpr e2') env) k
 evalExpr (Let x e0 e) env k = evalExpr (App (Lam x e) e0) env k
 evalExpr (LetRec x e0 e) env k = evalExpr (Let x (Rec (Lam x e)) e) env k
 --evalExpr (LetRec x y e0 e) env k = evalExpr e (insert x (VFun f) env) k
@@ -125,12 +127,12 @@ eval expr = evalExpr expr empty OK
 
 main :: IO ()
 main = do
-  let fib k = LetRec "fib" "n" (If (Lt (Var "n") (N 2))
+  let fib k = LetRec "fib" (Lam "n" (If (Lt (Var "n") (N 2))
         (Var "n")
         (Add
           (App (Var "fib") (Sub (Var "n") (N 1)))
           (App (Var "fib") (Sub (Var "n") (N 2)))
-        ) ) (App (Var "fib") (N k))
+        ) ) ) (App (Var "fib") (N k))
   print $ map (eval . fib) [0..15]
 
 
